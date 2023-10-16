550e8ea9037d9c131c81d99c0b0cc018b1e578ae
mamunit <mamun@w.mediaplanetus.com>
2023-10-16 21:39:24 +0600

    first

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main')]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/number', name: 'app_main')]
+    public function number(): Response
+    {
+        return new Response('<html><body>New App</body></html>');
+    }
+}
